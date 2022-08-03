d488e8190fed7c259253f18bbc738adb29f5d2c7
himani-gaikwad-dax <109571141+himani-gaikwad-dax@users.noreply.github.com>
2022-08-03 14:20:37 +0530

    Add files via upload

---
+++ DotNet/Models/Registration.cs
@@ -0,0 +1,10 @@
+﻿namespace Login_Application.Models
+{
+    public class Registration
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
